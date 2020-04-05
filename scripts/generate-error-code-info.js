d924809395ad9fa0416e3f086fedb434755a81ac
pavel-surinin <pavel.surinin@gmail.com>
2020-04-05 12:15:10 +0300

    updated script

--- scripts/generate-error-code-info.js
+++ scripts/generate-error-code-info.js
@@ -1,10 +1,10 @@
-const errors = require('../out/src/SerializerError').ERROR_CODE_REGISTRY
+const errors = require('../out/src/serializer/SerializerError').ERROR_CODE_REGISTRY
 const fs = require('fs')
 
 const header = '| Error Code | Error Description |'
 const lineDelimiter = '| --- | --- |'
 
-const codes = Object.values(errors).map((error) => `| ${error.code} | ${error.description} |`)
+const codes = Object.values(errors).map(error => `| ${error.code} | ${error.description} |`)
 
 const errorTable = [].concat(header).concat(lineDelimiter).concat(codes).join('\n')
 
